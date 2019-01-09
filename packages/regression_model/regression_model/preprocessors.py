ea12767f86b478f6b8811cf2bb8e34bb18e320df
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-09 21:17:22 +0000

    Section 6.2 - Initial setup

---
+++ packages/regression_model/regression_model/preprocessors.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class CategoricalImputer(BaseEstimator, TransformerMixin):
+    """Categorical data missing value imputer."""
+
+    def __init__(self, variables=None) -> None:
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None) -> 'CategoricalImputer':
+        """Fit statement to accomodate the sklearn pipeline."""
+
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Apply the transforms to the dataframe."""
+
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].fillna('Missing')
+
+        return X
