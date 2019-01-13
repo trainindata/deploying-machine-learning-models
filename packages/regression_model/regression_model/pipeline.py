53bc67c6a94e01f3fdaf05cbfa2b49465a0c7a1f
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-13 06:56:28 -0800

    Section 6.3 Connecting the Pipeline

--- packages/regression_model/regression_model/pipeline.py
+++ packages/regression_model/regression_model/pipeline.py
@@ -1,26 +1,41 @@
+from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
 
-import preprocessors as pp
+from regression_model.processing import preprocessors as pp
+from regression_model.config import config
 
-
-CATEGORICAL_VARS = ['MSZoning',
-                    'Neighborhood',
-                    'RoofStyle',
-                    'MasVnrType',
-                    'BsmtQual',
-                    'BsmtExposure',
-                    'HeatingQC',
-                    'CentralAir',
-                    'KitchenQual',
-                    'FireplaceQu',
-                    'GarageType',
-                    'GarageFinish',
-                    'PavedDrive']
-
-PIPELINE_NAME = 'lasso_regression'
 
 price_pipe = Pipeline(
     [
-        ('categorical_imputer',
-         pp.CategoricalImputer(variables=CATEGORICAL_VARS)),
-    ])
+        (
+            "categorical_imputer",
+            pp.CategoricalImputer(variables=config.CATEGORICAL_VARS_WITH_NA),
+        ),
+        (
+            "numerical_inputer",
+            pp.NumericalImputer(variables=config.NUMERICAL_VARS_WITH_NA),
+        ),
+        (
+            "temporal_variable",
+            pp.TemporalVariableEstimator(
+                variables=config.TEMPORAL_VARS, reference_variable=config.DROP_FEATURES
+            ),
+        ),
+        (
+            "rare_label_encoder",
+            pp.RareLabelCategoricalEncoder(tol=0.01, variables=config.CATEGORICAL_VARS),
+        ),
+        (
+            "categorical_encoder",
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS),
+        ),
+        ("log_transformer", pp.LogTransformer(variables=config.NUMERICALS_LOG_VARS)),
+        (
+            "drop_features",
+            pp.DropUnecessaryFeatures(variables_to_drop=config.DROP_FEATURES),
+        ),
+        ("scaler", MinMaxScaler()),
+        ("Linear_model", Lasso(alpha=0.005, random_state=0)),
+    ]
+)
