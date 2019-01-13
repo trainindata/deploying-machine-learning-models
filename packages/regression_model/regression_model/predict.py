85b13c87912d85627ffa13daeb5a7cc244e4c70e
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-13 09:52:20 -0800

    Section 6.5 - Data Validation

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from regression_model.processing.data_management import load_pipeline
 from regression_model.config import config
+from regression_model.processing.validation import validate_inputs
 
 
 pipeline_file_name = "regression_model.pkl"
@@ -13,7 +14,8 @@ def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
     data = pd.read_json(input_data)
-    prediction = _price_pipe.predict(data[config.FEATURES])
+    validated_data = validate_inputs(input_data=data)
+    prediction = _price_pipe.predict(validated_data[config.FEATURES])
     output = np.exp(prediction)
     response = {"predictions": output}
 
