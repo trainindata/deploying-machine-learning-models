ff5986dabb4c74cd746d422f16dbbf9566266147
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-13 06:56:28 -0800

    Section 6.3 Connecting the Pipeline

---
+++ packages/regression_model/regression_model/processing/preprocessors.py
@@ -0,0 +1,192 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class CategoricalImputer(BaseEstimator, TransformerMixin):
+    """Categorical data missing value imputer."""
+
+    def __init__(self, variables=None) -> None:
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None) -> "CategoricalImputer":
+        """Fit statement to accomodate the sklearn pipeline."""
+
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Apply the transforms to the dataframe."""
+
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].fillna("Missing")
+
+        return X
+
+
+class NumericalImputer(BaseEstimator, TransformerMixin):
+    """Numerical missing value imputer."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist mode in a dictionary
+        self.imputer_dict_ = {}
+        for feature in self.variables:
+            self.imputer_dict_[feature] = X[feature].mode()[0]
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inplace=True)
+        return X
+
+
+class TemporalVariableEstimator(BaseEstimator, TransformerMixin):
+    """Temporal variable calculator."""
+
+    def __init__(self, variables=None, reference_variable=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+        self.reference_variables = reference_variable
+
+    def fit(self, X, y=None):
+        # we need this step to fit the sklearn pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[self.reference_variables] - X[feature]
+
+        return X
+
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+    """Rare label categorical encoder"""
+
+    def __init__(self, tol=0.05, variables=None):
+        self.tol = tol
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            # frequent labels:
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(
+                X[feature].isin(self.encoder_dict_[feature]), X[feature], "Rare"
+            )
+
+        return X
+
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    """String to numbers categorical encoder."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y):
+        temp = pd.concat([X, y], axis=1)
+        temp.columns = list(X.columns) + ["target"]
+
+        # persist transforming dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            t = temp.groupby([var])["target"].mean().sort_values(ascending=True).index
+            self.encoder_dict_[var] = {k: i for i, k in enumerate(t, 0)}
+
+        return self
+
+    def transform(self, X):
+        # encode labels
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
+
+        # check if transformer introduces NaN
+        if X[self.variables].isnull().any().any():
+            null_counts = X[self.variables].isnull().any()
+            vars_ = {
+                key: value for (key, value) in null_counts.items() if value is True
+            }
+            raise ValueError(
+                f"Categorical encoder has introduced NaN when "
+                f"transforming categorical variables: {vars_.keys()}"
+            )
+
+        return X
+
+
+class LogTransformer(BaseEstimator, TransformerMixin):
+    """Logarithm transformer."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # to accomodate the pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        # check that the values are non-negative for log transform
+        if not (X[self.variables] > 0).all().all():
+            vars_ = self.variables[(X[self.variables] <= 0).any()]
+            raise ValueError(
+                f"Variables contain zero or negative values, "
+                f"can't apply log for vars: {vars_}"
+            )
+
+        for feature in self.variables:
+            X[feature] = np.log(X[feature])
+
+        return X
+
+
+class DropUnecessaryFeatures(BaseEstimator, TransformerMixin):
+    def __init__(self, variables_to_drop=None):
+        self.variables = variables_to_drop
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # encode labels
+        X = X.copy()
+        X = X.drop(self.variables, axis=1)
+
+        return X
--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -1,36 +1,40 @@
-import pathlib
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import joblib
+
+from regression_model import pipeline
+from regression_model.config import config
 
 
-PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
-TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
-DATASET_DIR = PACKAGE_ROOT / 'datasets'
-
-TESTING_DATA_FILE = DATASET_DIR / 'test.csv'
-TRAINING_DATA_FILE = DATASET_DIR / 'train.csv'
-TARGET = 'SalePrice'
-
-
-FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
-            'OverallCond', 'YearRemodAdd', 'RoofStyle', 'MasVnrType',
-            'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
-            '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
-            'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
-            'GarageCars', 'PavedDrive', 'LotFrontage',
-            # this variable is only to calculate temporal variable:
-            'YrSold']
-
-
-def save_pipeline() -> None:
+def save_pipeline(*, pipeline_to_persist) -> None:
     """Persist the pipeline."""
 
-    pass
+    save_file_name = "regression_model.pkl"
+    save_path = config.TRAINED_MODEL_DIR / save_file_name
+    joblib.dump(pipeline_to_persist, save_path)
+
+    print("saved pipeline")
 
 
 def run_training() -> None:
     """Train the model."""
 
-    print('Training...')
+    # read training data
+    data = pd.read_csv(config.DATASET_DIR / config.TRAINING_DATA_FILE)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.FEATURES], data[config.TARGET], test_size=0.1, random_state=0
+    )  # we are setting the seed here
+
+    # transform the target
+    y_train = np.log(y_train)
+
+    pipeline.price_pipe.fit(X_train[config.FEATURES], y_train)
+
+    save_pipeline(pipeline_to_persist=pipeline.price_pipe)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_training()
