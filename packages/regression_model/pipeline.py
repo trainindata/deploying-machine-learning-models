b7de0ff1e0f61a26c06b194ec9623bc6fbac3521
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-13 06:56:28 -0800

    Section 6.3 Connecting the Pipeline

--- packages/regression_model/pipeline.py
+++ packages/regression_model/pipeline.py
@@ -1,26 +1,56 @@
+from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 import preprocessors as pp
 
 
-CATEGORICAL_VARS = ['MSZoning',
-                    'Neighborhood',
-                    'RoofStyle',
-                    'MasVnrType',
-                    'BsmtQual',
-                    'BsmtExposure',
-                    'HeatingQC',
-                    'CentralAir',
-                    'KitchenQual',
-                    'FireplaceQu',
-                    'GarageType',
-                    'GarageFinish',
+# categorical variables with NA in train set
+CATEGORICAL_VARS_WITH_NA = [
+    'MasVnrType', 'BsmtQual', 'BsmtExposure',
+    'FireplaceQu', 'GarageType', 'GarageFinish'
+]
+
+TEMPORAL_VARS = 'YearRemodAdd'
+
+# this variable is to calculate the temporal variable,
+# can be dropped afterwards
+DROP_FEATURES = 'YrSold'
+
+# variables to log transform
+NUMERICALS_LOG_VARS = ['LotFrontage', '1stFlrSF', 'GrLivArea']
+
+# numerical variables with NA in train set
+NUMERICAL_VARS_WITH_NA = ['LotFrontage']
+
+# categorical variables to encode
+CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
+                    'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+                    'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
                     'PavedDrive']
 
-PIPELINE_NAME = 'lasso_regression'
 
 price_pipe = Pipeline(
     [
         ('categorical_imputer',
-         pp.CategoricalImputer(variables=CATEGORICAL_VARS)),
-    ])
+            pp.CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA)),
+        ('numerical_inputer',
+            pp.NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
+        ('temporal_variable',
+            pp.TemporalVariableEstimator(
+                variables=TEMPORAL_VARS,
+                reference_variable=TEMPORAL_VARS)),
+        ('rare_label_encoder',
+            pp.RareLabelCategoricalEncoder(
+                tol=0.01,
+                variables=CATEGORICAL_VARS)),
+        ('categorical_encoder',
+            pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
+        ('log_transformer',
+            pp.LogTransformer(variables=NUMERICALS_LOG_VARS)),
+        ('drop_features',
+            pp.DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+        ('scaler', MinMaxScaler()),
+        ('Linear_model', Lasso(alpha=0.005, random_state=0))
+    ]
+)
--- packages/regression_model/train_pipeline.py
+++ packages/regression_model/train_pipeline.py
@@ -1,5 +1,12 @@
 import pathlib
 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+
+import pipeline
+
 
 PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
@@ -20,16 +27,37 @@ FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
             'YrSold']
 
 
-def save_pipeline() -> None:
+def save_pipeline(*, pipeline_to_persist) -> None:
     """Persist the pipeline."""
 
-    pass
+    save_file_name = 'regression_model.pkl'
+    save_path = TRAINED_MODEL_DIR / save_file_name
+    joblib.dump(pipeline_to_persist, save_path)
+
+    print('saved pipeline')
 
 
 def run_training() -> None:
     """Train the model."""
 
-    print('Training...')
+    # read training data
+    data = pd.read_csv(TRAINING_DATA_FILE)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[FEATURES],
+        data[TARGET],
+        test_size=0.1,
+        random_state=0)  # we are setting the seed here
+
+    # transform the target
+    y_train = np.log(y_train)
+    y_test = np.log(y_test)
+
+    pipeline.price_pipe.fit(X_train[FEATURES],
+                            y_train)
+
+    save_pipeline(pipeline_to_persist=pipeline.price_pipe)
 
 
 if __name__ == '__main__':
