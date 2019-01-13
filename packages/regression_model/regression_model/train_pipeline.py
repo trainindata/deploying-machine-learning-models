9c8c9265b50fe1b3a95c07f8b27dbb6156086f4b
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-13 08:31:53 -0800

    Section 6.4 - Predictions and Testing Skeleton

--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -1,27 +1,16 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import joblib
 
 from regression_model import pipeline
+from regression_model.processing.data_management import load_dataset, save_pipeline
 from regression_model.config import config
-
-
-def save_pipeline(*, pipeline_to_persist) -> None:
-    """Persist the pipeline."""
-
-    save_file_name = "regression_model.pkl"
-    save_path = config.TRAINED_MODEL_DIR / save_file_name
-    joblib.dump(pipeline_to_persist, save_path)
-
-    print("saved pipeline")
 
 
 def run_training() -> None:
     """Train the model."""
 
     # read training data
-    data = pd.read_csv(config.DATASET_DIR / config.TRAINING_DATA_FILE)
+    data = load_dataset(file_name=config.TRAINING_DATA_FILE)
 
     # divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
