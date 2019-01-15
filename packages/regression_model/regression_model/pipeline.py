c7077c38fccc2301a3d59da1dd2df0c40ed21442
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-15 10:53:23 -0800

    Section 6.6 - Features

--- packages/regression_model/regression_model/pipeline.py
+++ packages/regression_model/regression_model/pipeline.py
@@ -3,6 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
 from regression_model.processing import preprocessors as pp
+from regression_model.processing import features
 from regression_model.config import config
 
 
@@ -30,7 +31,10 @@ price_pipe = Pipeline(
             "categorical_encoder",
             pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS),
         ),
-        ("log_transformer", pp.LogTransformer(variables=config.NUMERICALS_LOG_VARS)),
+        (
+            "log_transformer",
+            features.LogTransformer(variables=config.NUMERICALS_LOG_VARS),
+        ),
         (
             "drop_features",
             pp.DropUnecessaryFeatures(variables_to_drop=config.DROP_FEATURES),
