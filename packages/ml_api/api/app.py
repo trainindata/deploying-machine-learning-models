3ced939c1817d6d1c38bede816cf486b0ff018d8
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-18 13:16:07 -0800

    Section 7.3 - Setup Config and Logging

--- packages/ml_api/api/app.py
+++ packages/ml_api/api/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 
+from api.config import get_logger
 
-def create_app() -> Flask:
+
+_logger = get_logger(logger_name=__name__)
+
+
+def create_app(*, config_object) -> Flask:
     """Create a flask app instance."""
 
     flask_app = Flask('ml_api')
+    flask_app.config.from_object(config_object)
 
     # import blueprints
     from api.controller import prediction_app
     flask_app.register_blueprint(prediction_app)
+    _logger.debug('Application instance created')
 
     return flask_app
