b3e11731826242c55d861301ab3b4b9c01c7e9a3
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-18 13:16:07 -0800

    Section 7.3 - Setup Config and Logging

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, request
 
+from api.config import get_logger
+
+_logger = get_logger(logger_name=__name__)
+
 
 prediction_app = Blueprint('prediction_app', __name__)
 
@@ -7,4 +11,5 @@ prediction_app = Blueprint('prediction_app', __name__)
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
+        _logger.info('health status OK')
         return 'ok'
