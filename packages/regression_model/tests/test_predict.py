cc2e92e92adb16203a9dfbb332b9e8bd7e9efc3f
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-19 09:02:29 -0800

    Section 7.6 - API Schema Validation

--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -7,7 +7,7 @@ from regression_model.processing.data_management import load_dataset
 def test_make_single_prediction():
     # Given
     test_data = load_dataset(file_name='test.csv')
-    single_test_json = test_data[0:1].to_json(orient='records')
+    single_test_json = test_data[0:1]
 
     # When
     subject = make_prediction(input_data=single_test_json)
@@ -22,7 +22,7 @@ def test_make_multiple_predictions():
     # Given
     test_data = load_dataset(file_name='test.csv')
     original_data_length = len(test_data)
-    multiple_test_json = test_data.to_json(orient='records')
+    multiple_test_json = test_data
 
     # When
     subject = make_prediction(input_data=multiple_test_json)
