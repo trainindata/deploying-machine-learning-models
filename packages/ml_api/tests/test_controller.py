49b0aa3844e96a0aa488ef360aa43cbcfd297cde
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-19 04:08:54 -0800

    Section 7.5 - API Versioning

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -5,6 +5,8 @@ from regression_model import __version__ as _version
 import json
 import math
 
+from api import __version__ as api_version
+
 
 def test_health_endpoint_returns_200(flask_test_client):
     # When
@@ -12,6 +14,17 @@ def test_health_endpoint_returns_200(flask_test_client):
 
     # Then
     assert response.status_code == 200
+
+
+def test_version_endpoint_returns_version(flask_test_client):
+    # When
+    response = flask_test_client.get('/version')
+
+    # Then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    assert response_json['model_version'] == _version
+    assert response_json['api_version'] == api_version
 
 
 def test_prediction_endpoint_returns_prediction(flask_test_client):
