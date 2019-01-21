e23d50010674afcf60b264fd7b8c9d6060fa9e22
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-21 12:40:13 -0800

    Section 9.3 - Differential Tests in CI Part 1

--- packages/ml_api/tests/capture_model_predictions.py
+++ packages/ml_api/tests/capture_model_predictions.py
@@ -12,26 +12,23 @@ from regression_model.processing.data_management import load_dataset
 from api import config
 
 
-def capture_predictions(
-        *,
-        save_file: str = 'test_data_predictions.csv'):
+def capture_predictions() -> None:
     """Save the test data predictions to a CSV."""
 
+    save_file = 'test_data_predictions.csv'
     test_data = load_dataset(file_name='test.csv')
 
     # we take a slice with no input validation issues
-    multiple_test_json = test_data[99:600]
+    multiple_test_input = test_data[99:600]
 
-    predictions = make_prediction(input_data=multiple_test_json)
+    predictions = make_prediction(input_data=multiple_test_input)
 
     # save predictions for the test dataset
     predictions_df = pd.DataFrame(predictions)
 
     # hack here to save the file to the regression model
     # package of the repo, not the installed package
-    predictions_df.to_csv(
-        f'{config.PACKAGE_ROOT.parent}/'
-        f'regression_model/regression_model/datasets/{save_file}')
+    predictions_df.to_csv(f'{config.PACKAGE_ROOT}/{save_file}')
 
 
 if __name__ == '__main__':
