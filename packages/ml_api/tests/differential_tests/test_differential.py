4ffa40b85bc7f2bdba1c94091998f175f695dc63
ChristopherGS <christopher.samiullah@gmail.com>
2019-01-25 05:51:19 -0800

    Section 9.4 - Differential Tests in CI Part 2

--- packages/ml_api/tests/differential_tests/test_differential.py
+++ packages/ml_api/tests/differential_tests/test_differential.py
@@ -10,7 +10,6 @@ import pytest
 from api import config
 
 
-@pytest.mark.skip
 @pytest.mark.differential
 def test_model_prediction_differential(
         *,
@@ -25,7 +24,7 @@ def test_model_prediction_differential(
     previous_model_df = pd.read_csv(f'{config.PACKAGE_ROOT}/{save_file}')
     previous_model_predictions = previous_model_df.predictions.values
 
-    test_data = load_dataset(file_name=f'{save_file}')
+    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
     multiple_test_input = test_data[99:600]
 
     # When
