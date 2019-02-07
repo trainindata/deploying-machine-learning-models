4fb362ef66a81ef603c301620dc46ec7c0f07f90
ChristopherGS <christopher.samiullah@gmail.com>
2019-02-07 23:19:32 +0000

    update kaggle fetch script

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kaggle datasets download -d vbookshelf/v2-plant-seedlings-dataset -p packages/neural_network_model/neural_network_model/datasets/
+kaggle datasets download -d vbookshelf/v2-plant-seedlings-dataset -p packages/neural_network_model/neural_network_model/datasets/ && unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset
