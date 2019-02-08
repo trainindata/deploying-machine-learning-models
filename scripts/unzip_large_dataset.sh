54768545dbbd96bb6b7a9697ba9d0ec3da71cfb4
ChristopherGS <christopher.samiullah@gmail.com>
2019-02-08 08:51:09 +0000

    get publish step working

--- scripts/unzip_large_dataset.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset
