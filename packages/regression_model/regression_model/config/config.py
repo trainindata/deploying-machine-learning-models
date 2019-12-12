194b920ac35910bd7eabda679234fc8b0cc72aac
zahid1202 <zahid1202@gmail.com>
2019-12-12 04:40:32 -0800

    CircleCI Setup

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -6,8 +6,8 @@ import regression_model
 import pandas as pd
 
 
-pd.options.display.max_rows = 10
-pd.options.display.max_columns = 10
+#pd.options.display.max_rows = 10
+#pd.options.display.max_columns = 10
 
 
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
