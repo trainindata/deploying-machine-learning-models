808a5edc2b90de29a09f83a61c993f18ef792ed5
hung <lmh.hunglm@gmail.com>
2020-04-22 20:59:43 +0700

    adding test csv file

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p /home/circleci/project/packages/regression_model/regression_model/datasets/
-unzip /home/circleci/project/packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+#kaggle competitions download -c house-prices-advanced-regression-techniques -p /home/circleci/project/packages/regression_model/regression_model/datasets/
+#unzip /home/circleci/project/packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
