fbc30bd8ef8bb5db70b0004229af063577d3f884
hung <lmh.hunglm@gmail.com>
2020-04-22 20:50:28 +0700

    test unzip file

--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -6,9 +6,30 @@ from regression_model.processing.data_management import load_dataset
 
 def test_make_single_prediction():
     # Given
-    assert 1==1
+    test_data = load_dataset(file_name='test.csv')
+    single_test_json = test_data[0:1]
+
+    # When
+    subject = make_prediction(input_data=single_test_json)
+
+    # Then
+    assert subject is not None
+    assert isinstance(subject.get('predictions')[0], float)
+    assert math.ceil(subject.get('predictions')[0]) == 112476
 
 
 def test_make_multiple_predictions():
     # Given
-    assert 1==1
+    test_data = load_dataset(file_name='test.csv')
+    original_data_length = len(test_data)
+    multiple_test_json = test_data
+
+    # When
+    subject = make_prediction(input_data=multiple_test_json)
+
+    # Then
+    assert subject is not None
+    assert len(subject.get('predictions')) == 1451
+
+    # We expect some rows to be filtered out
+    assert len(subject.get('predictions')) != original_data_length
