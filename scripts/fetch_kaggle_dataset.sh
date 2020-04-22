6d1a7a2d7095445f899a86457255c0add6f1c388
hung <lmh.hunglm@gmail.com>
2020-04-22 20:54:58 +0700

    change

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-cd /packages/regression_model/regression_model/datasets/
-unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+kaggle competitions download -c house-prices-advanced-regression-techniques -p /home/circleci/project/packages/regression_model/regression_model/datasets/
+unzip /home/circleci/project/packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
