cf38c02adda2bb33a4b90c45d965d9a9dba3b8f4
hung <lmh.hunglm@gmail.com>
2020-04-23 11:39:59 +0700

    fixed kaggle download data

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-echo "==============Checking directory $(pwd)=================="
-dir
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-
-
 cd packages/regression_model/regression_model/datasets
-echo "========cd to=======$(pwd)"
-dir
-echo "========after unziped======"
 unzip house-prices-advanced-regression-techniques.zip
-dir
