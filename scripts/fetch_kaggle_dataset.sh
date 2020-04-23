54671d035047cc40ab32f3ce9c63ab2d72ace3be
hung <lmh.hunglm@gmail.com>
2020-04-23 11:07:16 +0700

    test

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-#kaggle competitions download -c house-prices-advanced-regression-techniques -p /home/circleci/project/packages/regression_model/regression_model/datasets/
+BASE_DIR=$(pwd)
+echo "Checking directory $(BASE_DIR)"
+
+kaggle competitions download -c house-prices-advanced-regression-techniques -p /packages/regression_model/regression_model/datasets/
 #unzip /home/circleci/project/packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+dir
