d67ba49a81f67d5c23af6db46b1296d50f3a63be
Michael Lohier <lohiermichael@gmail.com>
2020-07-05 11:43:15 +0800

    Add some comments

--- scripts/publish_model.sh
+++ scripts/publish_model.sh
@@ -4,6 +4,10 @@
 
 GEMFURY_URL=$PIP_EXTRA_INDEX_URL
 
+# Find the setup.py file
+# We will run: ./scripts/publish_model.sh ./packages/regression_model/
+# So we say that we should look for this file in ./packages/regression_model/
+
 set -e
 
 DIRS="$@"
@@ -26,9 +30,12 @@ build() {
     [ ! -e $SETUP ] && warn "No $SETUP file, skipping" && return
     PACKAGE_NAME=$(python $SETUP --fullname)
     echo "Package $PACKAGE_NAME"
+    # We generate the distribution: the trained model
     python "$SETUP" sdist bdist_wheel || die "Building package $PACKAGE_NAME failed"
+    # We loop over the distributed files
     for X in $(ls dist)
     do
+        # We upload them to Gemfury
         curl -F package=@"dist/$X" "$GEMFURY_URL" || die "Uploading package $PACKAGE_NAME failed on file dist/$X"
     done
 }
