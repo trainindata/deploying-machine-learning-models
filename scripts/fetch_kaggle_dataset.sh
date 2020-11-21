f680630d5895467ba11309aee18948b9fd55e505
Sam <woradee.s@gmail.com>
2020-11-21 15:16:05 +0700

    second try to solve FileNotFoundError in circleci

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,4 +2,8 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 
-unzip -o regression_model/datasets/house-prices-advanced-regression-techniques.zip -d regression_model/datasets
+unzip  packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+
+mv train.csv  packages/regression_model/regression_model/datasets/
+
+mv test.csv  packages/regression_model/regression_model/datasets/
