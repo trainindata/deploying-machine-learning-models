3a4dbd603b4cea6c0eec5be1b3b8f75f05c56397
David Lüttmann <david.luettmann@porsche.digital>
2020-12-14 12:51:08 +0100

    fix zip path

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip house-prices-advanced-regression-techniques.zip
+unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
