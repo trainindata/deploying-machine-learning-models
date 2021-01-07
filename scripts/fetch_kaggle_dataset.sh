acc541fc12e97818b325ab9d0e0de4144627bd91
marcmartinezcamarena@gmail.com <mmartinez@autis.es>
2021-01-07 19:10:16 +0100

    opening pr10

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip-aes -p "" packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+cd packages/regression_model/regression_model/datasets
+unzip-aes -p "" house-prices-advanced-regression-techniques.zip
+cd ..
+cd ..
+cd ..
+cd ..
