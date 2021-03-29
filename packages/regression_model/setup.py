99f7a8844dd0af282846fbad61e343a9c9093d62
AndreLuisCostaC <andreluiscostacarvalho@gmail.com>
2021-03-29 11:25:48 -0400

    increment regression model version

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -11,9 +11,9 @@ from setuptools import find_packages, setup
 # Package meta-data.
 NAME = 'regression_model'
 DESCRIPTION = 'Train and deploy regression model.'
-URL = 'your github project'
-EMAIL = 'your_email@email.com'
-AUTHOR = 'Your name'
+URL = 'https://github.com/AndreLuisCostaC/deploying-machine-learning-models'
+EMAIL = 'andreluiscostacarvalho@gmail.com'
+AUTHOR = 'AndreLuisCostaC'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
