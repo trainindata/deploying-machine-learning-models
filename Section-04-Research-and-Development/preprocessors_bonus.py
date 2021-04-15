d9c6c03f1e16acd579470146841dc4213734b803
Soledad Galli <solegalli@protonmail.com>
2021-04-15 15:55:41 +0200

    new notebooks for research env

---
+++ Section-04-Research-and-Development/preprocessors_bonus.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class NumericalImputer(BaseEstimator, TransformerMixin):
+    """Numerical missing value imputer."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist mode in a dictionary
+        self.imputer_dict_ = {}
+        for feature in self.variables:
+            self.imputer_dict_[feature] = X[feature].mean()
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inplace=True)
+        return X
+
+
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+    """Rare label categorical encoder"""
+
+    def __init__(self, tol=0.05, variables=None):
+        self.tol = tol
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            # frequent labels:
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(
+                X[feature].isin(self.encoder_dict_[feature]), X[feature], "Rare"
+            )
+
+        return X
+
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    """String to numbers categorical encoder."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y):
+        temp = pd.concat([X, y], axis=1)
+        temp.columns = list(X.columns) + ["target"]
+
+        # persist transforming dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            t = temp.groupby([var])["target"].mean().sort_values(ascending=True).index
+            self.encoder_dict_[var] = {k: i for i, k in enumerate(t, 0)}
+
+        return self
+
+    def transform(self, X):
+        # encode labels
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
+
+        return X
