6517a502e058a44d7e79b986d0b9e226057596b7
ChristopherGS <christopher.samiullah@gmail.com>
2021-05-14 20:09:46 +0100

    prepare logging

--- section-06-model-serving-api/house-prices-api/app/api.py
+++ section-06-model-serving-api/house-prices-api/app/api.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
+from loguru import logger
 from regression_model import __version__ as model_version
 from regression_model.predict import make_prediction
 
@@ -36,8 +37,13 @@ async def predict(input_data: schemas.MultipleHouseDataInputs) -> Any:
 
     # Advanced: You can improve performance of your API by rewriting the
     # `make prediction` function to be async and using await here.
+    logger.info(f"Making prediction on inputs: {input_data.inputs}")
     results = make_prediction(input_data=input_df.replace({np.nan: None}))
+
     if results["errors"] is not None:
+        logger.warning(f"Prediction validation error: {results.get('errors')}")
         raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
+
+    logger.info(f"Prediction results: {results.get('predictions')}")
 
     return results
--- section-06-model-serving-api/house-prices-api/app/config.py
+++ section-06-model-serving-api/house-prices-api/app/config.py
@@ -1,13 +1,14 @@
 import logging
 import sys
-from typing import List
+from types import FrameType
+from typing import List, cast
 
 from loguru import logger
 from pydantic import AnyHttpUrl, BaseSettings
 
 
 class LoggingSettings(BaseSettings):
-    LOGGING_LEVEL: int = logging.INFO  # logging levels are ints
+    LOGGING_LEVEL: int = logging.INFO  # logging levels are type int
 
 
 class Settings(BaseSettings):
@@ -31,11 +32,35 @@ class Settings(BaseSettings):
         case_sensitive = True
 
 
+# See: https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging  # noqa
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = str(record.levelno)
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
+            frame = cast(FrameType, frame.f_back)
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level,
+            record.getMessage(),
+        )
+
+
 def setup_app_logging(config: Settings) -> None:
     """Prepare custom logging for our application."""
+
     LOGGERS = ("uvicorn.asgi", "uvicorn.access")
+    logging.getLogger().handlers = [InterceptHandler()]
     for logger_name in LOGGERS:
         logging_logger = logging.getLogger(logger_name)
+        logging_logger.handlers = [InterceptHandler(level=config.logging.LOGGING_LEVEL)]
 
     logger.configure(
         handlers=[{"sink": sys.stderr, "level": config.logging.LOGGING_LEVEL}]
