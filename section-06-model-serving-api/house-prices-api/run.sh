9073282aab8e71e08dfdce29607e861ac87034ec
ChristopherGS <christopher.samiullah@gmail.com>
2021-05-14 14:53:21 +0100

    heroku setup

--- section-06-model-serving-api/house-prices-api/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export APP_MODULE=${APP_MODULE-app.main:app}
-export HOST=${HOST:-0.0.0.0}
-export PORT=${PORT:-8001}
-export LOG_LEVEL=${LOG_LEVEL:-debug}
-
-exec uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
