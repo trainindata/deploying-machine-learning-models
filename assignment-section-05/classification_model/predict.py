9eae7ae8075bd755c0de41d915b7cd6186516654
ChristopherGS <christopher.samiullah@gmail.com>
2022-01-09 15:06:03 +0000

    update validation

--- assignment-section-05/classification_model/predict.py
+++ assignment-section-05/classification_model/predict.py
@@ -3,10 +3,10 @@ import typing as t
 import numpy as np
 import pandas as pd
 
-from regression_model import __version__ as _version
-from regression_model.config.core import config
-from regression_model.processing.data_manager import load_pipeline
-from regression_model.processing.validation import validate_inputs
+from classification_model import __version__ as _version
+from classification_model.config.core import config
+from classification_model.processing.data_manager import load_pipeline
+from classification_model.processing.validation import validate_inputs
 
 pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
 _price_pipe = load_pipeline(file_name=pipeline_file_name)
