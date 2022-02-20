8c0771505dd81d20f6c6d4d5f6e2b5c83b216cc3
ChristopherGS <christopher.samiullah@gmail.com>
2022-02-20 21:19:45 +0000

    setup model comparison tests

--- section-09-differential-testing/model-package/differential_tests/conftest.py
+++ section-09-differential-testing/model-package/differential_tests/conftest.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 import pytest
 
 from regression_model.config.core import config
 from regression_model.processing.data_manager import load_dataset
 from regression_model import __version__ as model_version
 import os
+
+import pandas as pd
+
+PWD = Path.cwd()
 
 
 @pytest.fixture
@@ -17,3 +23,8 @@ def persist_results():
     label = os.environ.get("DIFF_TEST")
     with open(f"{label}_version.txt", "w") as handler:
         handler.write(model_version)
+
+
+@pytest.fixture
+def research_model_results() -> pd.DataFrame:
+    return pd.read_csv(PWD / "differential_tests"/ "research_model_test_results.csv")
---
+++ section-09-differential-testing/model-package/differential_tests/test_vs_research_env.py
@@ -0,0 +1,40 @@
+import math
+import os
+
+import numpy as np
+from regression_model.predict import make_prediction
+
+
+ACCEPTABLE_MODEL_DIFFERENCE = float(os.environ.get('ACCEPTABLE_MODEL_DIFFERENCE'))
+
+
+def test_make_prediction(research_model_results, sample_input_data):
+    # Given
+    expected_no_predictions = len(research_model_results)
+
+    # When
+    result = make_prediction(input_data=sample_input_data)
+
+    # Then
+    package_predictions = result.get("predictions")
+    assert isinstance(package_predictions, list)
+    assert isinstance(package_predictions[0], np.float64)
+    assert result.get("errors") is None
+    assert len(package_predictions) == expected_no_predictions
+
+    # Compare research vs. package predictions
+    for package_prediction, research_prediction in zip(
+        package_predictions, research_model_results.values
+    ):
+
+        # convert str to float.
+        previous_value = float(package_prediction)
+        current_value = float(research_prediction)
+
+        # rel_tol is the relative tolerance – it is the maximum allowed
+        # difference between a and b, relative to the larger absolute
+        # value of a or b. For example, to set a tolerance of 5%, pass
+        # rel_tol=0.05.
+        assert math.isclose(
+            previous_value, current_value, rel_tol=ACCEPTABLE_MODEL_DIFFERENCE
+        )
